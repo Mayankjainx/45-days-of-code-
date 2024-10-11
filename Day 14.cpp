fb42820d86f83a30a3d9ab54d16c9fea4e470e8e
Mayankjainx <144972772+Mayankjainx@users.noreply.github.com>
2024-10-11 17:51:25 +0530

    Day 14.cpp

---
+++ Day 14.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+       int t;
+
+       cin>>t;
+
+       while (t--){
+
+           int a, b, c, d;
+
+           cin>>a>>b>>c>>d;
+
+           cout<<max(a,b)+max(c,d)<<endl;
+
+           
+
+       }
+
+       return 0;
+
+
+
+}
